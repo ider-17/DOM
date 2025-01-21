a18caca9638d6d7a2856500c48fa6494b4ea39ba
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-21 13:07:24 +0800

    mini

--- balls.js
+++ balls.js
@@ -1,39 +1,20 @@
-
-function balls(num) {
-
-    for (let i = 1; i <= num; i++) {
-        const divElRef = document.createElement("div");
-        divElRef.innerText = i;
-        divElRef.style.color = "white";
-        divElRef.style.width = 50 + "px";
-        divElRef.style.height = 50 + "px";
-        divElRef.style.backgroundColor = "black";
-        divElRef.style.borderRadius = 50 + "%";
-        divElRef.style.alignContent = "center";
-        divElRef.style.textAlign = "center";
-
-        document.body.appendChild(divElRef);
-    }
-}
-balls(100);
-
-
-
-const setBg = () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    divElRef.style.backgroundColor = "#" + randomColor;
-    // color.innerHTML = "#" + randomColor;
-
-    //   genNew.addEventListener("click", setBg);
-    //   setBg();
-
-};
-
 const buttonElRef = document.createElement("button");
 buttonElRef.innerText = "Change-Bg";
-buttonElRef.style.backgroundColor = "green";
-buttonElRef.onclick = setBg();
 
-
-
+buttonElRef.onclick = setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    divElRef.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
 document.body.appendChild(buttonElRef);
+
+const divElRef = document.createElement("div");
+divElRef.innerText = "1";
+divElRef.style.width = 50 + "px";
+divElRef.style.height = 50 + "px";
+divElRef.style.backgroundColor = "black";
+divElRef.style.borderRadius = 50 + "%";
+divElRef.style.color = "white";
+divElRef.style.alignContent = "center";
+divElRef.style.textAlign = "center";
+document.body.appendChild(divElRef);
